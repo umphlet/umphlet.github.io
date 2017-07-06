fad41cdd6b7830351d3f4063fdb06acc604aeb61
umphlet <sean.umphlet@gmail.com>
2017-07-06 12:24:57 -0400

    Adding missing files

---
+++ webicons/js/scroll-animate.js
@@ -0,0 +1,14 @@
+$(function() {
+  $(".scroll").click(function(event) {
+      event.preventDefault();
+      var dest = 0;
+      if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+          dest = $(document).height() - $(window).height();
+      } else {
+          dest = $(this.hash).offset().top;
+      }
+      $('html,body').animate({
+          scrollTop: dest - 50
+      }, 1000, 'swing');
+  });
+});
